f7ccbecc1b864d73c6b8bf90fa90034b0f6713a9
farbod Aprin <aprin.farbod@gmail.com>
2018-07-26 17:40:26 +0200

    FBA2 / ClientV2 /fix the npm intall and compile bugs and lost packages

--- clientV2/node_modules/react-select/lib/utils/defaultArrowRenderer.js
+++ clientV2/node_modules/react-select/lib/utils/defaultArrowRenderer.js
@@ -1,24 +1,30 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-exports["default"] = arrowRenderer;
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-function arrowRenderer(_ref) {
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var arrowRenderer = function arrowRenderer(_ref) {
 	var onMouseDown = _ref.onMouseDown;
 
-	return _react2["default"].createElement("span", {
-		className: "Select-arrow",
+	return _react2.default.createElement('span', {
+		className: 'Select-arrow',
 		onMouseDown: onMouseDown
 	});
-}
+};
 
-;
-module.exports = exports["default"];
+arrowRenderer.propTypes = {
+	onMouseDown: _propTypes2.default.func
+};
+
+exports.default = arrowRenderer;
